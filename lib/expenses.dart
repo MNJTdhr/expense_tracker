b34fb07a4f6e7cb694d594f0c564537b0a080d72
JackDS <manojit.dhar.jack@gmail.com>
2024-12-27 23:19:45 +0530

    last change

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -8,14 +9,35 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "FlutterCourse",
+      amount: 69.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "biscuits",
+      amount: 169.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "cinema",
+      amount: 69.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.purple[900],
-          title: const Text("Expense Tracker"),
-        ),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.purple[900],
+        title: const Text("Expense Tracker"),
+      ),
       body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
